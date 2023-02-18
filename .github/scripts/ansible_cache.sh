76fc3b0a44ef75b86c8c49d62ae2d4f7d4f4d703
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-02-18 10:49:33 -0500

    ci(GITHUB): simplify ansible cache

--- .github/scripts/ansible_cache.sh
+++ .github/scripts/ansible_cache.sh
@@ -2,7 +2,7 @@
 
 # .github/scripts/ansible_cache.sh
 # Creates symlinks for attaching an external cache folder for Ansible.
-# Separate folders for Roles, and Collections are maintained.
+# Separate folders for Ansible-Compat, and Molecule are maintained.
 
 # 1: The absolute path of the mount point of the external cache folder.
 # 2: The absolute path of the usage point of the cache on the system.
@@ -16,9 +16,17 @@ main () {
   MOUNT_FOLDER="${1}"
   USAGE_FOLDER="${2}"
 
-  mkdir -p "${MOUNT_FOLDER}"
+  mkdir -p "${MOUNT_FOLDER}/ansible-compat"
+  mkdir -p "${MOUNT_FOLDER}/molecule"
 
-  ln -sf "${MOUNT_FOLDER}" "${USAGE_FOLDER}"
+  mkdir -p "${USAGE_FOLDER}"
+
+  ln -sf "${MOUNT_FOLDER}/ansible-compat" "${USAGE_FOLDER}/ansible-compat"
+  ln -sf "${MOUNT_FOLDER}/molecule" "${USAGE_FOLDER}/molecule"
+
+  echo "Cache Symlinks Created:"
+  echo "${MOUNT_FOLDER}/ansible-compat -> ${USAGE_FOLDER}/ansible-compat"
+  echo "${MOUNT_FOLDER}/molecule -> ${USAGE_FOLDER}/molecule"
 
 }
 
